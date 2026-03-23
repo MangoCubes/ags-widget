36241081afb4481debbac454aaf9e28ca55d221b
MangoCubes <10383115+MangoCubes@users.noreply.github.com>
2026-03-23 11:19:07 +0900

    Fix middle section not expanding when there are no players

--- widget/TopBar/TopCentre.tsx
+++ widget/TopBar/TopCentre.tsx
@@ -113,7 +113,7 @@ export const TopCentre: CallbackComp = () => {
 		class="musicText"
 	/>)
 	const comp = (
-		<box halign={Gtk.Align.CENTER}>
+		<box halign={Gtk.Align.CENTER} hexpand>
 			<With value={players}>
 				{(p) => {
 					if (p.length) return (
