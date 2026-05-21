ed8865e176c53e8e577fdfc92975b74addab4d7b
MangoCubes <10383115+MangoCubes@users.noreply.github.com>
2026-05-21 10:36:40 +0900

    Removing unused stuff

--- widget/TopBar/TopRight.tsx
+++ widget/TopBar/TopRight.tsx
@@ -1,11 +1,9 @@
 import WirePlumber from "gi://AstalWp";
 import AstalBattery from "gi://AstalBattery";
 import { IconCircular } from "../lib/IconCircular";
-import Brightness from "../bindings/brightness";
 import { Accessor, createBinding, createComputed, With } from "ags";
 import Gtk from "gi://Gtk";
 
-const brightness = Brightness.get_default()
 const wp = WirePlumber.get_default();
 const batt = AstalBattery.get_default();
 
@@ -17,22 +15,11 @@ const Battery = (b: AstalBattery.Device) => {
 
 	return (
 		<box><With value={batteryInfo}>{([percentage, isBattery, charging]) => {
-			const getNewBrightness = (up: boolean) => {
-				let newBrightness = brightness.screen;
-				if (up) newBrightness += 0.03;
-				else newBrightness -= 0.03;
-				if (newBrightness > 1) return 1;
-				if (newBrightness < 0.01) return 0.01;
-				else return newBrightness;
-			}
 			// Value above 0.95 is considered 100%
 			const adjusted = percentage * 100 > 95 ? 1 : percentage;
 			const icons = ["󰂎", "󰁺", "󰁻", "󰁼", "󰁽", "󰁾", "󰁿", "󰂀", "󰂁", "󰂂", "󰁹"];
 			const iconsCharging = ["󰢟", "󰢜", "󰂆", "󰂇", "󰂈", "󰢝", "󰂉", "󰢞", "󰂊", "󰂋", "󰂅"];
 			return (
-				// <EventBox
-				// 	onScroll={(_, event) => brightness.screen = getNewBrightness(event.delta_y < 0)}
-				// >
 				<IconCircular
 					iconClass="battery-icon"
 					circularClass="battery-progress"
@@ -40,7 +27,6 @@ const Battery = (b: AstalBattery.Device) => {
 					icon={isBattery ? ((charging ? iconsCharging : icons)[Math.floor(adjusted * 10)]) : "󰚥"}
 					textClass="battery-text"
 				/>
-				// </EventBox>
 			);
 
 		}}</With></box>
@@ -52,14 +38,6 @@ const Volume = (w: WirePlumber.Wp) => {
 	const vol = createBinding(speaker, "volume");
 
 	return <box><With value={vol}>{(volume) => {
-		const getNewVolume = (up: boolean) => {
-			let newVolume = volume;
-			if (up) newVolume += 0.03;
-			else newVolume -= 0.03;
-			if (newVolume > 1) return 1;
-			if (newVolume < 0) return 0;
-			else return newVolume;
-		}
 		const icons = ["󰝟", "󰕿", "󰖀", "󰕾", "󱄠", "󰸈"];
 		let icon;
 		if (volume === 0) icon = icons[0];
@@ -67,11 +45,7 @@ const Volume = (w: WirePlumber.Wp) => {
 		else if (volume < 0.66) icon = icons[2];
 		else if (volume < 1) icon = icons[3];
 		else icon = icons[4];
-		// else icon = icons[5];
 		return (
-			// <EventBox
-			// 	onScroll={(_, event) => speaker.set_volume(getNewVolume(event.delta_y < 0))}
-			// >
 			<IconCircular
 				iconClass="volume-icon"
 				circularClass="volume-progress"
@@ -79,7 +53,6 @@ const Volume = (w: WirePlumber.Wp) => {
 				icon={icon}
 				textClass="volume-text"
 			/>
-			// </EventBox>
 		);
 
 	}}</With></box>
